5636c62750051f45f5628703373616f4879aad65
adenadams20 <adamsdiene@gmail.com>
2025-11-24 17:16:08 +0000

    pre-last

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,28 @@
 
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
 import Register from './pages/Register'
+import Login from './pages/Login'
+
+
 import './App.css'
 
 export default function App() {
   return (
     <>
-    <Register/>
+      
+
+
+
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+
+
+
+
+
     </>
   )
 }
