68939f390d8ab651debd74fa0c12058babc617e0
mohamed-dione <mohameddione14@gmail.com>
2025-11-25 09:48:07 +0000

    testo

---
+++ vite-project/src/pages/Settings.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Settings() {
+  return (
+    <div className="container-fluid">
+      <h2 class="se">Parametres</h2>
+    </div>
+  )
+}
+
+export default Settings;
