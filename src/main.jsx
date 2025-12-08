5117ec9ae128fe92d8e29504e09c36232427c36e
adenadams20 <adamsdiene@gmail.com>
2025-12-08 12:14:31 +0000

    'remove'

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-    <BrowserRouter basename="/frontend1">
+    <BrowserRouter basename="/">
       
 
       <App />
